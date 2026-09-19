13e9aef7e4faee7060d41e0a31fa99546f03e248
ana-hern4ndez <ana.hernandez@factored.ai>
2026-09-19 14:12:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered '+ name_on_order+'!', icon="✅")
 
-
-
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
-        
