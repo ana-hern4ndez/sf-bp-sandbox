74310b38912bb0668e57b070a0ef748ce5744259
ana-hern4ndez <ana.hernandez@factored.ai>
2026-09-19 14:10:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,3 @@ smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/wa
 st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
         
-
-df_df
